721e654b130aeee98a6863dcda9a9b9b47826cd5
LinHeLurking <LinHe.Lurking@gmail.com>
2024-04-08 12:36:55 +0800

    inter;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- transpile.py
+++ transpile.py
@@ -1,69 +1,134 @@
-import re
-import sys
 import os
-from os import path as osp
+import re
 import shutil
+import sys
+from dataclasses import dataclass
+from os import path as osp
+from os import walk
+from typing import Dict, Union
 
-from obsidian_to_hugo import ObsidianToHugo
-
-
-def escape_latex_repl(match_obj: re.Match):
-    return "{{< raw >}}" + match_obj.group(0) + "{{< /raw >}}"
-
-
-def escape_latex(file_contents: str) -> str:
-    pattern = r"\${1,2}[^\$]+\${1,2}"
-    pattern = re.compile(pattern)
-    res = re.sub(pattern, escape_latex_repl, file_contents)
-    return res
+import yaml
 
 
-def rewrite_delete_line_repl(match_obj: re.Match):
-    return f"<del>{match_obj.group(0)}</del>"
+@dataclass
+class FileInfo:
+    original_path: str
+    meta: dict
+    content: str
+
+    def __post_init__(self):
+        self.original_path = osp.abspath(self.original_path)
 
 
-def rewrite_delete_line(file_contents: str) -> str:
-    pattern = r"~~[^~]+~~"
-    pattern = re.compile(pattern)
-    res = re.sub(pattern, rewrite_delete_line_repl, file_contents)
-    return res
-
-
-def filter_file(file_contents: str, file_path: str) -> bool:
-    # do something with the file path and contents
-    # print(file_path)
-    ignore = "hugo-hidden" in file_path or "Hugo Hidden" in file_path
-    ext = file_path.split(".")[-1]
-    ignore_ext = ("jpg", "png", "jpeg", "webp")
-    ignore = ignore or ext in ignore_ext
-    return not ignore
-
-
-def transpile(vault_path: str, hugo_root_path: str):
-    hugo_content_path = osp.join(hugo_root_path, "content")
-    if not osp.exists(hugo_content_path):
-        os.makedirs(hugo_content_path)
-    obsidian_to_hugo = ObsidianToHugo(
-        obsidian_vault_dir=vault_path,
-        hugo_content_dir=hugo_content_path,
-        filters=[filter_file],
-        processors=[escape_latex, rewrite_delete_line],
+class Transpiler:
+    LATEX_PATTERN = re.compile(r"\${1,2}[^\$]+\${1,2}")
+    DELETE_LINE_PATTERN = re.compile(r"~~[^~]+~~")
+    MD_LINK_PATTERN = re.compile(r"(!)?(\[.*\])(\(.*\))")
+    WIKI_LINK_PATTERN = re.compile(r"(!)?\[\[(.+)(\|.+)?\]\]")
+    IMAGE_EXT = (
+        ".jpg",
+        "jpeg",
+        ".png",
+        ".webp",
     )
 
-    obsidian_to_hugo.run()
+    def __init__(self, vault_path: str, hugo_root_path: str):
+        self.vault_path = vault_path
+        self.hugo_root_path = hugo_root_path
+        self.files: Dict[str, FileInfo] = {}
+        self.name2path: Dict[str, str] = {}
 
-    # copy images into static directory
-    image_src_path = osp.join(vault_path, "images")
-    image_dst_path = osp.join(hugo_root_path, "static")
-    image_dst_path = osp.join(image_dst_path, "images")
-    if osp.exists(image_dst_path):
-        shutil.rmtree(image_dst_path)
-    if osp.exists(image_src_path) and osp.isdir(image_src_path):
-        shutil.copytree(image_src_path, image_dst_path)
+    @classmethod
+    def parse_meta(cls, content: str) -> dict:
+        if content[:3] != "---":
+            return {}
+        idx = content.find("---", 3)
+        assert idx >= 3
+        meta = yaml.safe_load(content[3:idx])
+        return meta
+
+    @classmethod
+    def rewrite_rule_latex(cls, file_content: str) -> str:
+        def repl(match_obj: re.Match):
+            return "{{< raw >}}" + match_obj.group(0) + "{{< /raw >}}"
+
+        res = re.sub(cls.LATEX_PATTERN, repl, file_content)
+        return res
+
+    @classmethod
+    def rewrite_rule_delete_line(cls, file_content: str) -> str:
+        def repl(match_obj: re.Match):
+            return f"<del>{match_obj.group(0)}</del>"
+
+        res = re.sub(cls.DELETE_LINE_PATTERN, repl, file_content)
+        return res
+
+    def rewrite_rule_wiki_link(self, file_content: str) -> str:
+        def repl(match_obj: re.Match):
+            inline, target, display = match_obj.groups()
+            name_no_ext = target
+            if name_no_ext.endswith(".md"):
+                name_no_ext = name_no_ext[:-3]
+            return ""
+
+        res = re.sub(self.WIKI_LINK_PATTERN, repl, file_content)
+        return res
+
+    def accept_md(self, file_info: FileInfo) -> bool:
+        return True
+
+    def rewrite_md(self, file_info: FileInfo) -> FileInfo:
+        rewriters = (
+            self.rewrite_rule_latex,
+            self.rewrite_rule_delete_line,
+        )
+        for rewriter in rewriters:
+            file_info.content = rewriter(file_info.content)
+        return file_info
+
+    def load_file(self):
+        for dir_path, _, file_names in os.walk(self.vault_path):
+            if dir_path.startswith("."):
+                continue
+            for file_name in file_names:
+                self.name2path[file_name] = osp.join(dir_path, file_name)
+
+    def parse_md(self):
+        for file_name, file_path in self.name2path.items():
+            if not file_name.endswith(".md"):
+                continue
+            with open(file_path, "r") as f:
+                file_content = f.read()
+            file_meta = self.parse_meta(file_content)
+            file_info = FileInfo(file_path, file_meta, file_content)
+            if self.accept_md(file_info):
+                self.files[file_path] = self.rewrite_md(file_info)
+
+    def copy_images(self):
+        image_src_path = osp.join(self.vault_path, "images")
+        image_dst_path = osp.join(self.hugo_root_path, "static")
+        image_dst_path = osp.join(image_dst_path, "images")
+        if osp.exists(image_dst_path):
+            shutil.rmtree(image_dst_path)
+        if osp.exists(image_src_path) and osp.isdir(image_src_path):
+            shutil.copytree(image_src_path, image_dst_path)
+
+    def write_md(self):
+        pass
+
+    def transpile(self):
+        self.load_file()
+        self.parse_md()
+        self.write_md()
+        self.copy_images()
 
 
-if __name__ == "__main__":
+def main():
     assert len(sys.argv) >= 3, "<transpile.py> vault_path hugo_root_path"
     vault_path = sys.argv[1]
     hugo_root_path = sys.argv[2]
-    transpile(vault_path, hugo_root_path)
+    Transpiler(vault_path, hugo_root_path).transpile()
+
+
+if __name__ == "__main__":
+    main()
